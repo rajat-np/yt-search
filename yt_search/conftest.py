a587d084fd0b768515b3197c81b47576b8c585b2
Rajat Soni <rajat.tcp@gmail.com>
2021-01-16 17:35:53 +0530

    Remove redundent apps
    Remove users app and templates from the cookiecutter generated
    project.

--- yt_search/conftest.py
+++ yt_search/conftest.py
@@ -1,14 +1,14 @@
-import pytest
+# import pytest
 
-from yt_search.users.models import User
-from yt_search.users.tests.factories import UserFactory
-
-
-@pytest.fixture(autouse=True)
-def media_storage(settings, tmpdir):
-    settings.MEDIA_ROOT = tmpdir.strpath
+# from yt_search.users.models import User
+# from yt_search.users.tests.factories import UserFactory
 
 
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
+# @pytest.fixture(autouse=True)
+# def media_storage(settings, tmpdir):
+#     settings.MEDIA_ROOT = tmpdir.strpath
+
+
+# @pytest.fixture
+# def user() -> User:
+#     return UserFactory()
